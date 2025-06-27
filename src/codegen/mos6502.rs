ff1f2c9c2c3a6c85cb89b4801d13421302c8f1b3
miezekatze <miezmoon64@gmail.com>
2025-06-27 12:19:36 +0200

    remove unnecessary NOP

--- src/codegen/mos6502.rs
+++ src/codegen/mos6502.rs
@@ -1208,12 +1208,12 @@ pub unsafe fn generate_function(name: *const c_char, params_count: usize, auto_v
                 }
             },
             Op::Label{label} => {
-                // TODO: RE: https://github.com/tsoding/b/pull/147#issue-3154667157
+                // RE: https://github.com/tsoding/b/pull/147#issue-3154667157
                 // > For this thing I introduces a new NOP instruction because it would be a bit too
                 // > risky to just blindly jump on an address that could possibly be unused.
-                //
-                // Assess the risk and potentially remove this NOP
-                instr(out, NOP);
+                // NOP is not needed here, as this label is alwyas followed by another instructions,
+                // if it is the last in the function, there is *always* a RTS
+
                 da_append(&mut (*asm).op_labels, Label {
                     func_name: name,
                     label,
